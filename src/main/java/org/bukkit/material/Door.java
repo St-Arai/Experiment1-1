256eeff737f2ac0651c7a86a134b28f7883fcdad
Ryohei Takasawa <henshinfukanou@gmail.com>
2013-06-17 14:38:26 +0900

    Add default to switch statement.

--- src/main/java/org/bukkit/material/Door.java
+++ src/main/java/org/bukkit/material/Door.java
@@ -109,6 +109,9 @@ public class Door extends MaterialData implements Directional, Openable {
         case SOUTH:
             data |= 0x3;
             break;
+            
+        default:
+        	break;
         }
         setData(data);
     }
